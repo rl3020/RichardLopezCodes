5b7f27de7042b4ceeea933911d73c519a2b42898
Richard Lopez <richardlopez@dyn-160-39-10-33.dyn.columbia.edu>
2021-10-06 00:08:49 -0400

    added new route for no chaching server.puy

--- server.py
+++ server.py
@@ -6,6 +6,18 @@ import os
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route("/")
 def hello_world(): 
     return render_template("index.html")

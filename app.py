7d34b1dd19f23d0c3fa5f1a964f85895e6834e74
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-10-06 21:36:03 -0400

    removed no cache headers and changed env variables to reflect herokus env variables/config

--- app.py
+++ app.py
@@ -6,17 +6,7 @@ import os
 
 app = Flask(__name__)
 
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+
 
 @app.route("/")
 def hello_world(): 
@@ -37,7 +27,7 @@ def send_email():
     email = request.get_json()
     print("Email: ", email)
     credentials = get_credentials()
-    print(credentials)
+    print("creds: ", credentials)
 
     sender_email = credentials['sender_email']
     receiver_email = credentials['receiver_email']  # Enter receiver address
